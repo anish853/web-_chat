bd85dfc46816a2844d566641aeb520ef5b9e804e
ANISH BHARDWAJ <82315789+anish853@users.noreply.github.com>
2021-12-10 11:51:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const io = require('socket.io')(3000,{
+  cors:{
+    origin:"*"
+  }
+})
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', nam => {
+    users[socket.id] = nam
+    socket.broadcast.emit('user-connected', nam)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, nam: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+
