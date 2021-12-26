115d3f1ff914a1f9715f970fd50359d221e772e9
Anish Bhardwaj <bhardwajanish602@gmail.com>
2021-12-26 12:58:54 +0530

    second

---
+++ index.js
@@ -0,0 +1,16 @@
+const io = require('socket.io')(process.env.PORT || 3000,{
+  cors:{origin:"*"}});
+const users = {};
+io.on('connection', socket => {
+  socket.on('new-user', nam => {
+    users[socket.id] = nam
+    socket.broadcast.emit('user-connected', nam)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, nam: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
